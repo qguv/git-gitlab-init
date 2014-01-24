d9b2c1946c7b1f79284280732cf909e8356340b1
Quint Guvernator <quintus.public@gmail.com>
2014-01-24 15:05:19 -0500

    gofmt code

--- git-gitlab-init.go
+++ git-gitlab-init.go
@@ -1,21 +1,21 @@
 /*
 Initializes a repository remotely for GitLab-hosted origin servers.
- */
+*/
 
 package main
 
 import (
-    "os"
     "fmt"
     "github.com/docopt/docopt.go"
+    "os"
 )
 
 var (
-    version string = "0.1.0"
-    argsToParse []string = os.Args[1:]
-    automaticHelp bool = true
-    optionsFirst bool = true
-    helpstring string = `git gitlab-init
+    version       string   = "0.1.0"
+    argsToParse   []string = os.Args[1:]
+    automaticHelp bool     = true
+    optionsFirst  bool     = true
+    helpstring    string   = `git gitlab-init
 
 Create an empty Git repository on GitLab and locally.
 
