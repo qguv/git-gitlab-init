13812738abc06d6446525e9d3af08d4f9dee3cf4
Quint Guvernator <quintus.public@gmail.com>
2014-01-25 00:50:24 -0500

    write functions and update main for testing

--- git-gitlab-init.go
+++ git-gitlab-init.go
@@ -7,7 +7,10 @@ package main
 import (
     "fmt"
     "github.com/docopt/docopt.go"
+    "io/ioutil"
+    "net/http"
     "os"
+    "os/exec"
 )
 
 var (
@@ -34,10 +37,83 @@ Options:
 `
 )
 
+func runCommand(name string, arg ...string) (string, error) {
+    cmd := exec.Command(name, arg...)
+    raw_out, err := cmd.Output()
+    out := string(raw_out)
+    return out, err
+}
+
+func getSetting(name string) string {
+    cmd := exec.Command("git", "config", "--get", name)
+    raw_out, err := cmd.Output()
+    if err != nil {
+        panic(err)
+    }
+    return string(raw_out)
+}
+
+func sendGet(s string) string {
+    fmt.Println(s) //debug
+    resp, err := http.Get("http://example.com/")
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        panic(err)
+    }
+    return string(body)
+}
+
+/*
+   git init
+   touch README
+   git add README
+   git commit -m 'first commit'
+   git remote add origin http://src.nascifi.com:8080/quintus/tmp-testing.git
+   git push -u origin master
+*/
+
+func complainUndefined(options [][2]string) {
+    fmt.Println("Error! Your Gitlab API settings aren't defined.")
+    fmt.Println("Try running the following:\n")
+    var param [2]string
+    for _, param = range options {
+        setting, desc := param[0], param[1]
+        fmt.Println("    git config --global " + setting + " \"" + desc + "\"")
+    }
+    fmt.Println("")
+}
+
+func initialize(projectName string, user string, url string, dir string) (ok bool) {
+    runCommand("git", "init")
+
+    f, err := os.OpenFile("README.md", os.O_APPEND|os.O_WRONLY, 0600)
+    if err != nil {
+        panic(err)
+    }
+    defer f.Close()
+
+    _, err = f.WriteString("# " + projectName)
+    if err != nil {
+        panic(err)
+    }
+
+    runCommand("git", "add", "README.md")
+    runCommand("git", "commit", "-m", "initial commit")
+    runCommand("git", "remote", "add", "origin", "")
+    return true // ok
+}
+
 func main() {
     args, err := docopt.Parse(helpstring, argsToParse, automaticHelp, version, optionsFirst)
     if err != nil {
         panic(err)
     }
-    fmt.Println(args)
+    var options [][2]string
+    options = append(options, [2]string{"user.name", "First Last"})
+    options = append(options, [2]string{"gitlab.api", "v3"})
+    complainUndefined(options)
 }
