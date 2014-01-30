2ad77396c068e9b95d4efb84127e70e25e65733b
Quint Guvernator <quintus.public@gmail.com>
2014-01-29 23:21:47 -0500

    significantly improves documentation
    - most of the function parameter/internal argument names have changed for the better
    - added lots of docstrings

--- git-gitlab-init.go
+++ git-gitlab-init.go
@@ -15,13 +15,13 @@ import (
     "strings"
 )
 
+// Variables about gitlab-init
 var (
-    // Variables about this program
-    version       string   = "0.1.0"
-    argsToParse   []string = os.Args[1:]
-    automaticHelp bool     = true
-    optionsFirst  bool     = true
-    helpstring    string   = `git gitlab-init
+    version       string   = "0.1.1"
+    docopt_argument_source   []string = os.Args[1:]
+    docopt_autohelp_enabled bool     = true
+    docopt_optionsfirst_enabled  bool     = true
+    docopt_usage_pattern    string   = `git gitlab-init
 
 Create an empty Git repository on GitLab and locally.
 
@@ -49,6 +49,8 @@ Options:
 `
 )
 
+// findInSlice searches the given slice for a 2-array where element 0 equals the
+// given string and returns its index.
 func findInSlice(s string, slice [][2]string) int {
     for i, v := range slice {
         if v[0] == s {
@@ -58,6 +60,24 @@ func findInSlice(s string, slice [][2]string) int {
     return -1
 }
 
+// removeFromSlice removes the element with the given index from the given
+// slice and returns the resulting slice.
+func removeFromSlice(i int, slice [][2]string) [][2]string {
+    return append(slice[:i], slice[i+1:]...)
+}
+
+// removeGood removes the first 2-array it finds (in the given slice) where
+// element 0 equals the given string.
+func removeGood(s string, slice [][2]string) [][2]string {
+    i := findInSlice(s, slice)
+    if i != -1 {
+        return removeFromSlice(i, slice)
+    }
+    return [][2]string{}
+}
+
+// scrubUrl adds a trailing backslash to the given url if there isn't one
+// already.
 func scrubUrl(webaddress string) string {
     if webaddress[len(webaddress)-1] != "/"[0] {
         webaddress += "/"
@@ -65,14 +85,8 @@ func scrubUrl(webaddress string) string {
     return webaddress
 }
 
-func removeGood(s string, slice [][2]string) [][2]string {
-    i := findInSlice(s, slice)
-    if i != -1 {
-        return append(slice[:i], slice[i+1:]...)
-    }
-    return [][2]string{}
-}
-
+// runCommand executes a shell command and returns the resulting stdout as a
+// string and any errors as errors.
 func runCommand(name string, arg ...string) (string, error) {
     cmd := exec.Command(name, arg...)
     raw_out, err := cmd.Output()
@@ -80,41 +94,38 @@ func runCommand(name string, arg ...string) (string, error) {
     return out, err
 }
 
-/*
-   git init
-   touch README
-   git add README
-   git commit -m 'first commit'
-   git remote add origin http://src.nascifi.com:8080/quintus/tmp-testing.git
-   git push -u origin master
-*/
-
-func initialize(projectName string, user string, webaddress string) (ok bool) {
+// initialize initializes a git repository locally with a barebones README.md
+// and a basic first commit.
+func initialize(project_name string, origin_username string, origin_root_address string) (ok bool) {
     runCommand("git", "init")
 
     f, err := os.Create("README.md")
+    defer f.Close()
+    if err != nil {
+        panic(err)
+    }
+    _, err = f.WriteString("# " + project_name)
     if err != nil {
         panic(err)
     }
 
-    _, err = f.WriteString("# " + projectName)
-    if err != nil {
-        panic(err)
-    }
+    origin_root_address = scrubUrl(origin_root_address)
+    origin_relative_address := origin_username + "/" + project_name + ".git"
+    origin_full_address := origin_root_address + origin_relative_address
 
-    f.Close()
-
-    webaddress = scrubUrl(webaddress)
-    origin_webaddress := webaddress + user + "/" + projectName + ".git"
     runCommand("git", "add", "README.md")
     runCommand("git", "commit", "-m", "initial commit")
-    runCommand("git", "remote", "add", "origin", origin_webaddress)
+    runCommand("git", "remote", "add", "origin", origin_full_address)
     runCommand("git", "push", "-u", "origin", "master")
+
+    // #TODO: detect problems with runCommand and throw errors
+
     return true // ok
 }
 
-func sendPost(webaddress string, data url.Values) string {
-    resp, err := http.PostForm(webaddress, data)
+// sendPost sends a HTTP POST request to the given url with given payload.
+func sendPost(address string, payload url.Values) string {
+    resp, err := http.PostForm(address, payload)
     if err != nil {
         panic(err)
     }
@@ -126,127 +137,141 @@ func sendPost(webaddress string, data url.Values) string {
     return string(body)
 }
 
-func apiCommand(root string, webaddress string, data url.Values, token string, apiVersion string) string {
-    root = scrubUrl(root)
-    request := root + "api/" + apiVersion + "/" + webaddress
-    request += "?private_token=" + token
-    return sendPost(request, data)
+// apiCommand sends a HTTP POST request containing the given payload to the
+// Gitlab instance with the given API credentials.
+func apiCommand(gitlab_root_address string, api_subdirectory string, payload url.Values, api_token string, gitlab_api_version string) string {
+    gitlab_root_address = scrubUrl(gitlab_root_address)
+    api_request_address := gitlab_root_address + "api/" + gitlab_api_version + "/" + api_subdirectory
+    api_request_address += "?private_token=" + api_token
+    return sendPost(api_request_address, payload)
 }
 
-func makeRemoteRepo(root string, name string, description string, token string, apiVersion string, protection string) string {
-    data := make(url.Values)
-    data.Set("name", name)
-    if description != "" {
-        data.Set("description", description)
+// makeRemoteRepo initializes a repo to the Gitlab instance with the given API
+// credentials.
+func makeRemoteRepo(gitlab_root_address string, repo_name string, repo_description string, api_token string, gitlab_api_version string, repo_permissions string) string {
+    payload := make(url.Values)
+    payload.Set("name", repo_name)
+    if repo_description != "" {
+        payload.Set("description", repo_description)
     }
-    if protection == "private" {
-        data.Set("visibility_level", "0")
-    } else if protection == "public" {
-        data.Set("visibility_level", "20")
-    } else if protection == "internal" {
-        data.Set("visibility_level", "10")
+    if repo_permissions == "private" {
+        payload.Set("visibility_level", "0")
+    } else if repo_permissions == "public" {
+        payload.Set("visibility_level", "20")
+    } else if repo_permissions == "internal" {
+        payload.Set("visibility_level", "10")
     } else {
-        return "ERROR! in protection" // #DEBUG
+        return "ERROR! in repo_permissions" // #DEBUG
     }
-    return apiCommand(root, "projects", data, token, apiVersion)
+    return apiCommand(gitlab_root_address, "projects", payload, api_token, gitlab_api_version)
 }
 
-func getSetting(setting string) (out string, err error) {
-    cmd := exec.Command("git", "config", "--get", setting)
-    raw_out, err := cmd.Output()
-    out = strings.Trim(string(raw_out), "\n")
+// getSetting scrapes `git config` for the given setting and outputs its value.
+func getSetting(setting string) (value string, err error) {
+    setting_cmd := exec.Command("git", "config", "--get", setting)
+    raw_value, err := setting_cmd.Output()
+    value = strings.Trim(string(raw_value), "\n")
     return
 }
 
-func softGetSetting(setting string, desc string, badOptions [][2]string) (out string, newBadOptions [][2]string) {
-    out, err := getSetting(setting)
+// softGetSetting scrapes `git config` for the given setting and outputs its
+// value if it exists; otherwise the option and a short description are added
+// to a running slice of undefined options.
+func softGetSetting(setting string, setting_help_description string, bad_options [][2]string) (value string, new_bad_options [][2]string) {
+    value, err := getSetting(setting)
     if err != nil {
-        newBadOptions = append(badOptions, [2]string{setting, desc})
+        new_bad_options = append(bad_options, [2]string{setting, setting_help_description})
     } else {
-        newBadOptions = badOptions
+        new_bad_options = bad_options
     }
     return
 }
 
-func complainUndefined(options [][2]string) {
+// complainUndefined prints a pretty error message explaining how to remedy
+// configuration issues à la the official git subcommands.
+func complainUndefined(bad_options [][2]string) {
     fmt.Println("Error! Your Gitlab API settings aren't defined.")
     fmt.Println("Try running the following:\n")
     var param [2]string
-    for _, param = range options {
+    for _, param = range bad_options {
         setting, desc := param[0], param[1]
         fmt.Println("    git config --global " + setting + " \"" + desc + "\"")
     }
     fmt.Println("")
 }
 
-func varsFromGitConfig() (username string, root string, apiVersion string, token string, badOptions [][2]string) {
-    username, badOptions = softGetSetting("gitlab.username", "gitlabusername", badOptions)
-    root, badOptions = softGetSetting("gitlab.url", "http://my.gitlab.instance/", badOptions)
-    root = scrubUrl(root)
-    apiVersion, badOptions = softGetSetting("gitlab.api", "v3", badOptions)
-    token, badOptions = softGetSetting("gitlab.token", "your_gitlab_token", badOptions)
+// varsFromGitConfig scrapes `git config` for all the necessary gitlab API
+// authentication information
+func varsFromGitConfig() (gitlab_username string, gitlab_root_address string, gitlab_api_version string, gitlab_api_token string, bad_options [][2]string) {
+    gitlab_username, bad_options = softGetSetting("gitlab.username", "gitlabusername", bad_options)
+    gitlab_root_address, bad_options = softGetSetting("gitlab.url", "http://my.gitlab.instance/", bad_options)
+    gitlab_root_address = scrubUrl(gitlab_root_address)
+    gitlab_api_version, bad_options = softGetSetting("gitlab.api", "v3", bad_options)
+    gitlab_api_token, bad_options = softGetSetting("gitlab.token", "your_gitlab_token", bad_options)
     return
 }
 
-func main() { //testing
-    args, err := docopt.Parse(helpstring, argsToParse, automaticHelp, version, optionsFirst)
+func main() {
+    args, err := docopt.Parse(docopt_usage_pattern, docopt_argument_source, docopt_autohelp_enabled, version, docopt_optionsfirst_enabled)
     if err != nil {
         panic(err)
     }
 
+    // reflect debug flag as bool and store in debug
     debug := args["--debug"].(bool)
 
-    username, root, apiVersion, token, badOptions := varsFromGitConfig()
+    // Scraping config files for API credentials
+    gitlab_username, gitlab_root_address, gitlab_api_version, gitlab_api_token, bad_options := varsFromGitConfig()
     if debug {
         fmt.Println(args)
     }
 
     // Overriding config parameters from docopt
-    if username_opt, ok := args["-u"].(string); ok {
-        username = username_opt
-        badOptions = removeGood("gitlab.username", badOptions)
+    if gitlab_username_opt, ok := args["-u"].(string); ok {
+        gitlab_username = gitlab_username_opt
+        bad_options = removeGood("gitlab.username", bad_options)
     }
-    if webaddress_opt, ok := args["-l"].(string); ok {
-        root = webaddress_opt
-        badOptions = removeGood("gitlab.url", badOptions)
+    if gitlab_root_address_opt, ok := args["-l"].(string); ok {
+        gitlab_root_address = gitlab_root_address_opt
+        bad_options = removeGood("gitlab.url", bad_options)
     }
     if api_opt, ok := args["-v"].(string); ok {
-        apiVersion = api_opt
-        badOptions = removeGood("gitlab.api", badOptions)
+        gitlab_api_version = api_opt
+        bad_options = removeGood("gitlab.api", bad_options)
     }
-    if token_opt, ok := args["-t"].(string); ok {
-        token = token_opt
-        badOptions = removeGood("gitlab.token", badOptions)
+    if gitlab_api_token_opt, ok := args["-t"].(string); ok {
+        gitlab_api_token = gitlab_api_token_opt
+        bad_options = removeGood("gitlab.token", bad_options)
     }
 
-    root = scrubUrl(root)
+    gitlab_root_address = scrubUrl(gitlab_root_address)
 
-    if len(badOptions) != 0 {
-        complainUndefined(badOptions)
+    if len(bad_options) != 0 {
+        complainUndefined(bad_options)
         return
     }
 
     // Setting repo settings from docopt
-    var description string
-    if description_opt, ok := args["-d"].(string); ok {
-        description = description_opt
+    var repo_description string
+    if repo_description_opt, ok := args["-d"].(string); ok {
+        repo_description = repo_description_opt
     } else {
-        description = ""
+        repo_description = ""
     }
-    name := args["<repository>"].(string)
-    protection := args["-p"].(string)
+    repo_name := args["<repository>"].(string)
+    repo_permissions := args["-p"].(string)
 
     if debug {
-        fmt.Println("username:", username)
-        fmt.Println("api version:", apiVersion)
-        fmt.Println("token:", token)
-        fmt.Println("url:", root)
+        fmt.Println("username:", gitlab_username)
+        fmt.Println("api version:", gitlab_api_version)
+        fmt.Println("token:", gitlab_api_token)
+        fmt.Println("url:", gitlab_root_address)
     }
 
-    response := makeRemoteRepo(root, name, description, token, apiVersion, protection)
+    response := makeRemoteRepo(gitlab_root_address, repo_name, repo_description, gitlab_api_token, gitlab_api_version, repo_permissions)
     if debug {
         fmt.Println(response)
     }
 
-    initialize(name, username, root)
+    initialize(repo_name, gitlab_username, gitlab_root_address)
 }
