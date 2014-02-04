c000e23b2cdbff1c22b53bb4ac7065353f35ed51
Quint Guvernator <quintus.public@gmail.com>
2014-02-03 23:10:15 -0500

    bump version to 0.1.3

--- git-gitlab-init.go
+++ git-gitlab-init.go
@@ -18,7 +18,7 @@ import (
 
 // Variables about gitlab-init
 var (
-    version                     string   = "0.1.2"
+    version                     string   = "0.1.3"
     docopt_argument_source      []string = os.Args[1:]
     docopt_autohelp_enabled     bool     = true
     docopt_optionsfirst_enabled bool     = true
